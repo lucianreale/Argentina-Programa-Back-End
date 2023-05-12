a15280c3385efcd549fb0da6d3694fe4d05987b5
porce <porce@LAPTOP-OHJSH5LP>
2023-05-12 15:03:01 -0300

    primer commit, user terminado y Education en progreso

---
+++ src/main/java/com/portfolioBackEnd/portfolioBackEnd/service/IUserService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolioBackEnd.portfolioBackEnd.service;
+import java.util.List;
+
+import com.portfolioBackEnd.portfolioBackEnd.model.User;
+
+/**
+ *
+ * @author porce
+ */
+public interface IUserService {
+    public List<User> getUser();
+    
+    public void addUser (User usu);
+    
+    public void deleteUser (Long id);
+    
+    public User getUserByid (Long id);
+}
