b10f8b82ac72d184054de923048781b29c25d721
porce <porce@LAPTOP-OHJSH5LP>
2023-05-13 15:44:37 -0300

    construccion de education

--- src/main/java/com/portfolioBackEnd/portfolioBackEnd/service/IEducationService.java
+++ src/main/java/com/portfolioBackEnd/portfolioBackEnd/service/IEducationService.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.portfolioBackEnd.portfolioBackEnd.service;
+import java.util.List;
 
+import com.portfolioBackEnd.portfolioBackEnd.model.Education;
 /**
  *
  * @author porce
  */
 public interface IEducationService {
+    
+    public List<Education> getEducation();
+    
+    public void addEducation (Education edu);
+    
+    public void deleteEducation (Long id);
+    
+    public List<Education> getEducationByUser (Long id);
     
 }
